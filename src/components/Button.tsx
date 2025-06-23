96cb8dbb1a132e1c83105586592e5a5efbb96fa9
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2025-06-23 22:52:44 +0700

    feat: extension login page ui design implementation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { record } from "../constants/images";
+import { record, google } from "../constants/images";
 type Props = {
   color: string;
   text: string;
@@ -8,20 +8,37 @@ type Props = {
 
 const Button = (props: Props) => {
   return (
-    <div>
+    <>
       <button
         onClick={props.onClick}
         type="button"
-        className={props.color === "primary" ? "btn-primary" : "btn-secondary"}
+        className={
+          props.color == "primary" || props.color == "google"
+            ? "btn-primary max-w-[300px]"
+            : "btn-secondary max-w-[300px]"
+        }
       >
-        <div className="flex items-center justify-center gap-1">
+        <div
+          className={`flex items-center justify-center gap-1 relative ${
+            props.color === "google" ? "p-1" : ""
+          }`}
+        >
           {props.color === "primary" && (
             <img src={record} alt={`${props.text} button`} />
+          )}
+          {props.color === "google" && (
+            <div className=" bg-white absolute left-0 rounded-full p-1">
+              <img
+                src={google}
+                alt={`${props.text} button`}
+                className="w-6 h-6"
+              />
+            </div>
           )}
           {props.text}
         </div>
       </button>
-    </div>
+    </>
   );
 };
 
