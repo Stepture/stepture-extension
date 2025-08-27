23f00f6315993334759a0f9e7241dee35630dd55
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2025-08-27 19:01:40 +0700

    issue#19: sign in with google button confussion issue

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -2,4 +2,5 @@ import record from "../assets/images/record.svg";
 import stepture from "../assets/images/stepture-logo.svg";
 import google from "../assets/images/devicon_google.svg";
 import stepture_round from "../assets/images/stepture_logo_round.svg";
-export { record, stepture, google, stepture_round };
+import right_arrow from "../assets/images/right-arrow.png";
+export { record, stepture, google, stepture_round, right_arrow };
